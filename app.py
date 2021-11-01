ec9e43bde586d459f482bacc969da658d6abc18b
knight2995 <knight2995@kakao.com>
2021-11-01 18:41:52 +0900

    Init

---
+++ app.py
@@ -0,0 +1,13 @@
+
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Init Test'
+
+if __name__ == "__main__":
+    app.run('0.0.0.0', port=5000, debug=True)
+
+
