a966dad1e937a4371f1279a93e1916d16b9f2cd0
knight2995 <knight2995@kakao.com>
2021-11-02 20:29:29 +0900

    명언 제조기 v1 구현 + config 수정

--- app.py
+++ app.py
@@ -1,26 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_restx import Api
+from flask_cors import CORS
 
 app = Flask(__name__,
             static_url_path='',
             static_folder='web/static',
             )
+
+app.config.from_object('config.DevelopmentConfig')
+
+# 시간 관계상 모두 허용
+CORS(app)
+
 api = Api(app, version='0.1', title='API', description='PythonFlaskWebAPI')
 
 # Controller 등록
-from api.Controller.testController import TestNS
-
-api.add_namespace(TestNS, '/todos')
-
 from api.Controller.wise_saying_controller import wise_saying_namespace
 
 api.add_namespace(wise_saying_namespace, '/wise-saying')
 
 
-@app.route('/')
-def hello():
-    return 'Init Test'
+# 테스트를 위한 더미 등록
+@app.route("/명언제조기")
+def 명언제조기():
+    return render_template('feature1.html', base_url=app.config['BASE_URL'])
 
 
 if __name__ == "__main__":
-    app.run('127.0.0.1', port=5000, debug=True)
+    app.run('0.0.0.0', port=5000)
