bd8140a1d7f7c1ad199247e5fc1abf0e18e22ec0
knight2995 <knight2995@kakao.com>
2021-11-02 15:04:50 +0900

    명언 제조기 v1 구현

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 from flask import Flask
-from flask_restx import Resource, Api
+from flask_restx import Api
 
-app = Flask(__name__)
+app = Flask(__name__,
+            static_url_path='',
+            static_folder='web/static',
+            )
 api = Api(app, version='0.1', title='API', description='PythonFlaskWebAPI')
 
 # Controller 등록
 from api.Controller.testController import TestNS
+
 api.add_namespace(TestNS, '/todos')
+
+from api.Controller.wise_saying_controller import wise_saying_namespace
+
+api.add_namespace(wise_saying_namespace, '/wise-saying')
 
 
 @app.route('/')
