c6683c285c729f4f0276804b6c32cd52367d3523
knight2995 <knight2995@kakao.com>
2021-11-02 20:48:06 +0900

    명언 제조기 v1 구현 + config 수정

--- app.py
+++ app.py
@@ -27,4 +27,4 @@ def 명언제조기():
 
 
 if __name__ == "__main__":
-    app.run('0.0.0.0', port=5000)
+    app.run('0.0.0.0', port=app.config['PORT'])
--- config.py
+++ config.py
@@ -5,8 +5,10 @@ class Config(object):
 class DevelopmentConfig(Config):
     DEBUG = True
     BASE_URL = 'http://127.0.0.1:5000'
+    PORT = 5000
 
 
 class DeployConfig(Config):
     DEBUG = False
     BASE_URL = 'http://knight2995.site'
+    PORT = 80
