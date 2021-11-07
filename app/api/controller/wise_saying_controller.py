ab62c902e6ccf168dbf40a7e970b6dcf481f4629
knight2995 <knight2995@kakao.com>
2021-11-07 10:18:02 +0900

    dicom_viewer 특정 어트리뷰트가 없을 경우 예외 처리

--- app/api/controller/wise_saying_controller.py
+++ app/api/controller/wise_saying_controller.py
@@ -3,28 +3,33 @@ from flask_restx import Resource, Namespace, reqparse
 
 from app.api.service.wise_saying_service import make_wise_saying
 
-
 wise_saying_namespace = Namespace(
     name="명언짤 제조기",
-    description="명언짤 제조를 위한 API.",
+    description="명언짤 제조를 위한 API",
 )
+
+parser = reqparse.RequestParser()
+parser.add_argument('file', location='files', type=werkzeug.datastructures.FileStorage,
+                    help='사진 파일',
+                    required=True)
+parser.add_argument('text', required=True, help='삽입할 명언(한글 기준)')
 
 
 @wise_saying_namespace.route('/')
-@wise_saying_namespace.response(200, 'Found')
-@wise_saying_namespace.response(404, 'Not found')
+@wise_saying_namespace.response(200, 'Success')
 @wise_saying_namespace.response(500, 'Internal Error')
 class WiseSaying(Resource):
     @wise_saying_namespace.doc('post')
+    @wise_saying_namespace.expect(parser)
     def post(self):
-        parser = reqparse.RequestParser()
-        parser.add_argument('file', location='files',
-                            type=werkzeug.datastructures.FileStorage, required=True)
-        parser.add_argument('text', required=True)
 
         args = parser.parse_args()
 
         file_object = args['file']
-        wise_saying = make_wise_saying(file_object, args['text'])
 
-        return wise_saying, 200
+        try:
+            wise_saying = make_wise_saying(file_object, args['text'])
+            return wise_saying, 200
+
+        except Exception:
+            return '', 500
--- app/api/service/dicom_viewer_service.py
+++ app/api/service/dicom_viewer_service.py
@@ -20,13 +20,6 @@ def convert_dicom_image_to_png(file: werkzeug.datastructures.FileStorage, type: 
     ds: pydicom.FileDataset = pydicom.read_file(file)
 
     pixel_array = ds.pixel_array  # dicom image
-    # Rescale_slope = ds.RescaleSlope  # dicom header (Rescale slope)
-    # Rescale_intercept = ds.RescaleIntercept  # dicom header (Rescale intercept)
-    # Window_center = ds.WindowCenter  # dicom header (Window center)
-    # Window_width = ds.WindowWidth  # dicom header (Window width)
-    # Photometric_interpretation = ds.PhotometricInterpretation  # dicom header (Photometric interpretation)
-
-    # windowed = apply_voi_lut(ds.pixel_array, ds)
 
     image = exposure.equalize_adapthist(pixel_array)
 
@@ -40,34 +33,32 @@ def convert_dicom_image_to_png(file: werkzeug.datastructures.FileStorage, type: 
     elif type == 'all':
         tags = get_tags_all(ds)
 
-
     return json.dumps({"imgData": str(base64.b64encode(buffer))[2:-1], 'tags': tags})
 
 
 def get_tags(ds: pydicom.FileDataset) -> dict:
     tags = dict()
 
+    tags['PatientName'] = str(ds.PatientName) if hasattr(ds, 'PatientName') else ''
+    tags['PatientID'] = str(ds.PatientID) if hasattr(ds, 'PatientID') else ''
+    tags['PatientBirthDate'] = str(ds.PatientBirthDate) if hasattr(ds, 'PatientBirthDate') else ''
+    tags['PatientSex'] = str(ds.PatientSex) if hasattr(ds, 'PatientSex') else ''
+    tags['PatientAge'] = str(ds.PatientAge) if hasattr(ds, 'PatientAge') else ''
 
-    tags['PatientName'] = str(ds.PatientName)
-    tags['PatientID'] = str(ds.PatientID)
-    tags['PatientBirthDate'] = str(ds.PatientBirthDate)
-    tags['PatientSex'] = str(ds.PatientSex)
-    tags['PatientAge'] = str(ds.PatientAge)
+    tags['StudyDate'] = str(ds.StudyDate) if hasattr(ds, 'StudyDate') else ''
+    tags['StudyTime'] = str(ds.StudyTime) if hasattr(ds, 'StudyTime') else ''
+    tags['StudyID'] = str(ds.StudyID) if hasattr(ds, 'StudyID') else ''
+    tags['Modality'] = str(ds.Modality) if hasattr(ds, 'Modality') else ''
+    tags['StudyDescription'] = str(ds.StudyDescription) if hasattr(ds, 'StudyDescription') else ''
 
-    tags['StudyDate'] = str(ds.StudyDate)
-    tags['StudyTime'] = str(ds.StudyTime)
-    tags['StudyID'] = str(ds.StudyID)
-    tags['Modality'] = str(ds.Modality)
-    tags['StudyDescription'] = str(ds.StudyDescription)
-
-    tags['SeriesDate'] = str(ds.SeriesDate)
-    tags['SeriesTime'] = str(ds.SeriesTime)
-    tags['SeriesDescription'] = str(ds.SeriesDescription)
+    tags['SeriesDate'] = str(ds.SeriesDate) if hasattr(ds, 'SeriesDate') else ''
+    tags['SeriesTime'] = str(ds.SeriesTime) if hasattr(ds, 'SeriesTime') else ''
+    tags['SeriesDescription'] = str(ds.SeriesDescription) if hasattr(ds, 'SeriesDescription') else ''
 
     return tags
 
-def get_tags_all(ds: pydicom.FileDataset) -> list:
 
+def get_tags_all(ds: pydicom.FileDataset) -> list:
     tags = list()
 
     data = str(ds)
@@ -84,21 +75,19 @@ def get_tags_all(ds: pydicom.FileDataset) -> list:
         tag.append({'(Value)': v[49:]})
         tags.append(tag)
 
-    #print(meta_tags)
+    # print(meta_tags)
 
     # Simplify implement
     for v in meta_tags.splitlines()[1:]:
-        group_element = v[:v.find(')')+1]
-        tag_description = v[v.find(')')+2:v.find(':')-2]
-        value = v[v.find(':')-2:]
+        group_element = v[:v.find(')') + 1]
+        tag_description = v[v.find(')') + 2:v.find(':') - 2]
+        value = v[v.find(':') - 2:]
 
         tag = list()
         tag.append({'(Group,Element)': group_element})
         tag.append({'(TAG Description)': tag_description})
         tag.append({'(Value)': value})
         tags.append(tag)
-
-
 
     # 안 씀
     # # tags[''] = str(ds.file_meta.)
@@ -158,7 +147,5 @@ def get_tags_all(ds: pydicom.FileDataset) -> list:
     # tags['SeriesDate'] = str(ds.SeriesDate)
     # tags['SeriesTime'] = str(ds.SeriesTime)
     # tags['SeriesDescription'] = str(ds.SeriesDescription)
-
-
 
     return tags
