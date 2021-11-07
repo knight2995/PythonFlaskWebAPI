6d68b9fd673e05cf14d63828c7976183decc1618
knight2995 <knight2995@kakao.com>
2021-11-07 11:37:18 +0900

    APi 문서를 위한 작업 및 예외 처리추가 -1

--- app/api/service/auth_service.py
+++ app/api/service/auth_service.py
@@ -5,8 +5,12 @@ from app.api.repository.user_repository import user_repository
 # 회원 가입
 def check_id_pw(_user: user.User):
     # DAO 거치던가
-    user = user_repository.find_user_by_user_id(_user.user_id)
-    if user.password == _user.password:
+    find_user = user_repository.find_user_by_user_id(_user.user_id)
+
+    if find_user is None:
+        return False
+
+    if find_user.password == _user.password:
         return user
     else:
         return False
