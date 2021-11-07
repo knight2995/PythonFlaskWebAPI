966083f911e6d61520e7c88a599415fe76214ade
knight2995 <knight2995@kakao.com>
2021-11-07 14:30:10 +0900

    API 문서 수정

--- app/api/controller/photo_controller.py
+++ app/api/controller/photo_controller.py
@@ -4,6 +4,8 @@ from flask_restx import Resource, Namespace, reqparse
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from app.api.service.photo_service import photo_upload, photo_download, find_all_photos, find_photo_data
 
+from app.api.custom_exception.common_exception import ForbiddenException
+
 photoNS = Namespace(
     name="photo",
     description="사진을 올리고 다운로드 하고 보는 컨트롤러",
@@ -11,12 +13,23 @@ photoNS = Namespace(
 
 
 @photoNS.route("/<int:photo_idx>")
+@photoNS.response(200, 'Success')
+@photoNS.response(403, 'Forbidden')
+@photoNS.response(500, 'Internal Error')
 class Photo(Resource):
 
     @jwt_required()
     def get(self, photo_idx):
-        ret = find_photo_data(photo_idx)
-        return ret, 200
+
+        try:
+            ret = find_photo_data(photo_idx)
+            return ret, 200
+
+        except ForbiddenException as e:
+            return str(e), 403
+
+        except Exception as e:
+            return '', 500
 
 
 parser = reqparse.RequestParser()
@@ -25,25 +38,33 @@ parser.add_argument('file', location='files',
 
 
 @photoNS.route("")
+@photoNS.response(200, 'Success')
+@photoNS.response(403, 'Forbidden')
+@photoNS.response(500, 'Internal Error')
 class Photos(Resource):
+    post_parser = reqparse.RequestParser()
+    post_parser.add_argument('file', location='files',
+                             type=werkzeug.datastructures.FileStorage, required=True)
+    post_parser.add_argument('album_idx', required=True)
+
+    get_parser = reqparse.RequestParser()
+    get_parser.add_argument('file', location='files',
+                             type=werkzeug.datastructures.FileStorage, required=True)
+    get_parser.add_argument('album_idx', required=True)
 
     @jwt_required()
+    @photoNS.expect(post_parser)
     def post(self):
-        parser = reqparse.RequestParser()
-        parser.add_argument('file', location='files',
-                            type=werkzeug.datastructures.FileStorage, required=True)
-        parser.add_argument('album_idx', required=True)
 
-        args = parser.parse_args()
+        args = self.post_parser.parse_args()
 
         photo_upload(get_jwt_identity(), args['file'], args['album_idx'])
 
         return 'ok', 200
 
     @jwt_required()
+    @photoNS.expect(get_parser)
     def get(self):
-        parser = reqparse.RequestParser()
-        parser.add_argument('album_idx', required=True)
 
         args = parser.parse_args()
 
--- app/api/custom_exception/common_exception.py
+++ app/api/custom_exception/common_exception.py
@@ -1,5 +1,3 @@
-
-
 class UserDuplicatedException(Exception):
     def __init__(self):
         super().__init__('이미 존재하는 id 입니다.')
@@ -7,9 +5,18 @@ class UserDuplicatedException(Exception):
     def __str__(self):
         return '이미 존재하는 id 입니다.'
 
+
 class AlbumDuplicatedException(Exception):
     def __init__(self):
-        super().__init__('이미 존재하는 id 입니다.')
+        super().__init__('이미 존재하는 이름 입니다.')
 
     def __str__(self):
-        return '이미 존재하는 id 입니다.'
+        return '이미 존재하는 이름 입니다.'
+
+
+class ForbiddenException(Exception):
+    def __init__(self):
+        super().__init__('잘못된 접근입니다.')
+
+    def __str__(self):
+        return '잘못된 접근입니다.'
